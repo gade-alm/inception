9e1ee205dc629ae21414c0a4f67f6ce4e580fbca
gade-alm <gabrieldealmeidatorres@gmail.com>
2024-02-28 21:54:18 +0000

    updates

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -2,7 +2,10 @@ service mariadb start;
 
 sleep 2;
 
-# mysql -u root -e "CREATE DATABASE $DBNAME;"
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MARIADATABASE;"
+# mysql -u root -e "CREATE USER IF NOT EXISTS $MARIAUSER IDENTIFIED BY $MARIAPASSWORD;"
+# mysql -u root -e "GRANT ALL PRIVILENGES ON $MARIADATABASE TO $MARIAUSER IDENTIFIED BY $MARIAPASSWORD;"
+# mysql -u root -e FLUSH PRIVILEGES; 
 # service mariadb stop;
 
 # exec mysqld_safe --bind-address=localhost
