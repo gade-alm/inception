e7622a7fd72d5a4e093989054122338aeafeb9db
gade-alm <gabrieldealmeidatorres@gmail.com>
2024-03-13 22:55:41 +0000

    some changes on envs

--- srcs/requirements/wordpress/conf/wp-cli-conf.sh
+++ srcs/requirements/wordpress/conf/wp-cli-conf.sh
@@ -1,6 +1,13 @@
-wp core download --allow-root
-
-wp config create --allow-root --path=/var/www/wordpress	\
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+	cd /var/www/html && wp core download --allow-root &&
+	wp config create --allow-root --path=/var/www/wordpress	\
 			--dbname=$SQL_DATABASE \
 			--dbuser=$SQL_USER \
 			--dbpass=$SQL_PASSWORD \
+			--dbhost=mariadb
+			&&
+	wp core install --url=${WORDPRESS_URL}/wordpress --title=${WORDPRESS_TITLE} --admin-user=${WORDPRESS_USER} --admin-password=${WORDPRESS_PASSWORD} --admin-email=${WORDPRESS_EMAIL} --allow-root
+else
+	echo "THE DB ALREADY EXISTS!"
+	
+fi
