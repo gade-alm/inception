06750a1abcc77105b4946e6455c4432f300ae6ae
Gabriel <gabrieldealmeidatorres@gmail.coom>
2024-03-15 13:42:44 +0000

    wordpress working again!

--- srcs/requirements/wordpress/conf/wp-cli-conf.sh
+++ srcs/requirements/wordpress/conf/wp-cli-conf.sh
@@ -1,13 +1,12 @@
-if [ ! -f "/var/www/html/wp-config.php" ]; then
-	cd /var/www/html && wp core download --allow-root &&
-	wp config create --allow-root --path=/var/www/wordpress	\
-			--dbname=$SQL_DATABASE \
-			--dbuser=$SQL_USER \
-			--dbpass=$SQL_PASSWORD \
-			--dbhost=mariadb
-			&&
+#!/bin/bash	
+	wp cli update --yes --allow-root
+	wp core download --allow-root
+	wp config create	--allow-root \
+											--dbname=$SQL_DATABASE \
+											--dbuser=$SQL_USER \
+											--dbpass=$SQL_PASSWORD \
+											--dbhost=mariadb:3306
 	wp core install --url=${WORDPRESS_URL}/wordpress --title=${WORDPRESS_TITLE} --admin-user=${WORDPRESS_USER} --admin-password=${WORDPRESS_PASSWORD} --admin-email=${WORDPRESS_EMAIL} --allow-root
-else
-	echo "THE DB ALREADY EXISTS!"
-	
-fi
+	mkdir -p /run/php/
+	exec /usr/sbin/php-fpm7.4 -F -R
+
