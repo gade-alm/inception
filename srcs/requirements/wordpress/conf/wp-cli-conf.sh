e4cfd131f9b4d91e12313002e0bdace2c50d092a
Gabriel <gabrieldealmeidatorres@gmail.coom>
2024-03-15 14:06:38 +0000

    check mariadb connection with workdpress

--- srcs/requirements/wordpress/conf/wp-cli-conf.sh
+++ srcs/requirements/wordpress/conf/wp-cli-conf.sh
@@ -6,7 +6,7 @@
 											--dbuser=$SQL_USER \
 											--dbpass=$SQL_PASSWORD \
 											--dbhost=mariadb:3306
-	wp core install --url=${WORDPRESS_URL}/wordpress --title=${WORDPRESS_TITLE} --admin-user=${WORDPRESS_USER} --admin-password=${WORDPRESS_PASSWORD} --admin-email=${WORDPRESS_EMAIL} --allow-root
+	wp core install --url=${WORDPRESS_URL}/wordpress --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --allow-root
 	mkdir -p /run/php/
 	exec /usr/sbin/php-fpm7.4 -F -R
 
